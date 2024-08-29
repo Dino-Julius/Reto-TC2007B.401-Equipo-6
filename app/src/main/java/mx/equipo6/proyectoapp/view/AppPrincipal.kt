8921b5549cb72e66315d179ed12292a640ed2a90
Julio Vivas <jcvmlego@gmail.com>
2024-08-29 13:44:31 -0600

    Barras de navegación funcionales,

--- app/src/main/java/mx/equipo6/proyectoapp/view/AppPrincipal.kt
+++ app/src/main/java/mx/equipo6/proyectoapp/view/AppPrincipal.kt
@@ -1,20 +1,35 @@
 package mx.equipo6.proyectoapp.view
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -36,7 +51,10 @@ fun AppPrincipal(homeVM: HomeVM, aboutUsVM: AboutUsVM, modifier: Modifier = Modi
     val navController = rememberNavController()
     RetoAppTheme {
         Scaffold(
-            topBar = { AppTopBar() },
+            topBar = { AppTopBar(
+                onLeftButtonClick = { Log.d("check", "Botón del lado izquierdo") },
+                onRightButtonClick = { Log.d("check", "Botón del lado derecho") }
+            ) },
             bottomBar = { AppBottomBar(navController) }
         ) { innerPadding ->
             AppNavHost(
@@ -51,49 +69,108 @@ fun AppPrincipal(homeVM: HomeVM, aboutUsVM: AboutUsVM, modifier: Modifier = Modi
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppTopBar() {
-    TopAppBar(
-        title = {
-            Text(text = "The GOATS APP",
-                textAlign = TextAlign.Center,
-                style = MaterialTheme.typography.headlineMedium,
-                modifier = Modifier.fillMaxWidth()
+fun AppTopBar(
+    onLeftButtonClick: () -> Unit,  // Acción para el botón izquierdo
+    onRightButtonClick: () -> Unit  // Acción para el botón derecho
+) {
+    Surface(
+        color = MaterialTheme.colorScheme.primary, // Color de fondo
+        shape = MaterialTheme.shapes.medium.copy(bottomEnd = CornerSize(16.dp), bottomStart = CornerSize(16.dp)), // Esquinas inferiores redondeadas
+        modifier = Modifier.fillMaxWidth().height(105.dp)
+    ) {
+        TopAppBar(
+            title = {
+                Box(
+                    modifier = Modifier.fillMaxWidth().padding(top = 25.dp),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "ZAZIL",
+                        style = MaterialTheme.typography.headlineLarge,
+                        textAlign = TextAlign.Center,
+                        color = Color.White
+                    )
+                }
+            },
+            navigationIcon = {
+                IconButton(
+                    onClick = onLeftButtonClick,
+                    modifier = Modifier.padding(top = 25.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.Home, // Puedes cambiar el ícono
+                        contentDescription = "Menú",
+                        tint = Color.White,
+                        modifier = Modifier.size(200.dp)
+                    )
+                }
+            },
+            actions = {
+                IconButton(
+                    onClick = onRightButtonClick,
+                    modifier = Modifier.padding(top = 25.dp)
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle, // Puedes cambiar el ícono
+                        contentDescription = "Configuración",
+                        tint = Color.White,
+                        modifier = Modifier.size(200.dp)
+                    )
+                }
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0xFFD5507C) // Hacer transparente el color de fondo del TopAppBar
             )
-        }
-    )
+        )
+    }
 }
 
 @Composable
 fun AppBottomBar(navController: NavHostController) {
-    BottomAppBar{
-        val pilaNavegación by navController.currentBackStackEntryAsState()
-        val pantallaActual = pilaNavegación?.destination
+    Surface(
+        color = MaterialTheme.colorScheme.primary, // Color de fondo
+        shape = MaterialTheme.shapes.medium.copy(topEnd = CornerSize(16.dp), topStart = CornerSize(16.dp)), // Esquinas superiores redondeadas
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        BottomAppBar(
+            containerColor = Color(0xFFD5507C) // Hacer transparente el color de fondo del BottomAppBar
 
-        // Generar el menú inferior
-        Pantallas.listaPantallas.forEach { pantalla ->
-            NavigationBarItem(selected = pantalla.ruta == pantallaActual?.route,
-                onClick = {
-                    navController.navigate(pantalla.ruta) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+        ) {
+            val navigationPile by navController.currentBackStackEntryAsState()
+            val actualScreen = navigationPile?.destination
+
+            // Generar el menú inferior
+            Windows.listaPantallas.forEach { pantalla ->
+                NavigationBarItem(
+                    selected = pantalla.route == actualScreen?.route,
+                    onClick = {
+                        navController.navigate(pantalla.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }},
-                label = { Text(text = pantalla.etiqueta) },
-                icon = {
-                    Icon(imageVector = pantalla.icono,
-                        contentDescription = pantalla.etiqueta)
-                },
-                alwaysShowLabel = true)
-//            // Si la pantalla actual es igual a la pantalla actual, se pone en negrita
-//            val estilo = if (pantalla.ruta == pantallaActual) MaterialTheme.typography.bodyBold else MaterialTheme.typography.body1
-//            Text(
-//                text = pantalla.etiqueta,
-//                style = estilo,
-//                modifier = Modifier.weight(1f),
-//                textAlign = TextAlign.Center
-//            )
+                    },
+                    label = { Text(text = pantalla.etiqueta) },
+                    icon = {
+                        Icon(
+                            imageVector = pantalla.icono,
+                            contentDescription = pantalla.etiqueta,
+                            modifier = Modifier.size(35.dp)
+                        )
+                    },
+                    alwaysShowLabel = true,
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = Color.Black, // Color del ícono seleccionado
+                        selectedTextColor = Color.White, // Color del texto seleccionado
+                        indicatorColor = Color.White, // Color del indicador
+                        unselectedIconColor = Color.White, // Color del ícono no seleccionado
+                        unselectedTextColor = Color.White // Color del texto no seleccionado
+
+                    )
+                )
+            }
         }
     }
 }
@@ -107,20 +184,20 @@ fun AppNavHost(
 ) {
     NavHost(
         navController = navController,
-        startDestination = Pantallas.ROUTE_HOME,
+        startDestination = Windows.ROUTE_HOME,
         modifier = modifier.fillMaxSize()) {
         // Lista de pantallas de la aplicación
-        composable(Pantallas.ROUTE_ABOUTUS) {
+        composable(Windows.ROUTE_ABOUTUS) {
             AboutUsView(modifier, aboutUsVM)
         }
 //        composable(Pantallas.RUTA_CHATBOT) {
 //            ChatBotView(modifier, chatBotVM)
 //        }
-        composable(Pantallas.ROUTE_HOME) {
+        composable(Windows.ROUTE_HOME) {
             HomeView(modifier, homeVM)
         }
         // Botón Comunidad
-        composable(Pantallas.ROUTE_COMUNITY) {
+        composable(Windows.ROUTE_COMUNITY) {
             ComunityView(modifier)
         }
         // Botón Calendario
--- app/src/main/java/mx/equipo6/proyectoapp/view/Windows.kt
+++ app/src/main/java/mx/equipo6/proyectoapp/view/Windows.kt
@@ -9,8 +9,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 /**
  * Clase sellada que contiene las pantallas de la aplicación.
  */
-sealed class Pantallas(
-    val ruta: String,
+sealed class Windows(
+    val route: String,
     val etiqueta: String,
     val icono: ImageVector
 ) {
@@ -18,24 +18,28 @@ sealed class Pantallas(
     companion object {
         var listaPantallas = listOf(AboutUsView, ComunityView, HomeView)
         // Constante en tiempo de compilación (#Define)
-        const val ROUTE_HOME = "Home"
+        const val ROUTE_HOME = "Inicio"
         const val ROUTE_ABOUTUS = "¿Quiénes Somos?"
         const val ROUTE_COMUNITY = "Comunidad"
+        const val ROUTE_CONFIG = "Configuración"
+        const val ROUTE_CHATBOT = "ChatBot"
+        const val ROUTE_CALENDAR = "Calendario"
+        const val ROUTE_STORE = "Tienda"
     }
 
-    private data object AboutUsView : Pantallas(
+    private data object AboutUsView : Windows(
         ROUTE_ABOUTUS,
         "¿Quiénes Somos?",
         Icons.Default.ThumbUp
     )
 
-    private data object ComunityView : Pantallas(
+    private data object ComunityView : Windows(
         ROUTE_COMUNITY,
         "Comunidad",
         Icons.Default.Person
     )
 
-    private data object HomeView : Pantallas(
+    private data object HomeView : Windows(
         ROUTE_HOME,
         "HOME",
         Icons.Default.Home
